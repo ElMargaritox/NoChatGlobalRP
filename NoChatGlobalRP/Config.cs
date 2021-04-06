9487382f7799dd6bc34816c3b24f0a5d853272ed
ElMargaritox <alexisguzman0303@gmail.com>
2021-04-06 18:36:13 -0300

    PRUEBA

---
+++ NoChatGlobalRP/Config.cs
@@ -0,0 +1,18 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoChatGlobalRP
+{
+  public  class Config : IRocketPluginConfiguration
+    {
+        public string mensaje;
+        public void LoadDefaults()
+        {
+            mensaje = "No Puedes Escribir En Global";
+        }
+    }
+}
